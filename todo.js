60f8966764357e346af7557ceb437ba01294ca8f
shaghayegh momeni <shghygh.momeni18@gmail.com>
2022-10-24 12:09:09 +0330

    updated to last version of practice

--- todo.js
+++ todo.js
@@ -38,12 +38,19 @@ const taskTime = document.getElementById("taskTime");
 const addTaskBtn = document.getElementById("addTaskBtn");
 const todo = document.querySelector(".to-do");
 const weekDays = document.querySelectorAll(".weekDays");
-//[0,1,2,3,4,5,6]
+const countTasks = document.querySelectorAll(".count");
+const editTaskModal = document.querySelector(".edit-task-modal");
+const editedTaskTitle = document.getElementById("edited-task-title");
+const editedTaskTime = document.getElementById("edited-task-time");
+const editSubmitBtn = document.getElementById("edit-submit");
+
+let dayIndex;
+let todoIndex;
+
 let weekDayName = "shanbe";
 showTask(storageTaskInfo[0].task);
 
 addTaskBtn.addEventListener("click", addTask);
-// checkBox.addEventListener("click", taskDone);
 
 //Functions
 function currentDayTasks(event) {
@@ -70,13 +77,14 @@ weekDays.forEach((weekday) => {
 function showTask(taskItems) {
   todo.innerHTML = "";
   taskItems.forEach((task) => {
-    creatTask(task.taskName, task.taskTime);
+    creatTask(task.taskName, task.taskTime, task.isDone, task.id);
   });
 }
 
-function creatTask(taskName, taskTime) {
+function creatTask(taskName, taskTime, isDone, todoId) {
   let todo_list_div = document.createElement("div");
   todo_list_div.classList.add("to-do-list");
+  isDone && todo_list_div.classList.add("done");
   todo.append(todo_list_div);
 
   let todo_item_rigth_div = document.createElement("div");
@@ -86,7 +94,7 @@ function creatTask(taskName, taskTime) {
   let todo_item_rigth_svg = document.createElement("div");
   todo_item_rigth_svg.classList.add("to-do-item-rigth-svg");
   todo_item_rigth_div.append(todo_item_rigth_svg);
-  todo_item_rigth_svg.onclick = taskDone;
+  todo_item_rigth_svg.onclick = () => taskDone(todoId);
 
   todo_item_rigth_svg.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                 class="checkbox" viewBox="0 0 16 16">
@@ -109,18 +117,27 @@ function creatTask(taskName, taskTime) {
   todo_item_left_timeinput.innerText = taskTime;
   todo_item_left_div.append(todo_item_left_timeinput);
 
-  todo_item_left_div.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+  let todo_item_left_div_edit = document.createElement("div");
+  todo_item_left_div.append(todo_item_left_div_edit);
+  todo_item_left_div_edit.onclick = () => editTask(todoId, taskName, taskTime);
+
+  let todo_item_left_div_trash = document.createElement("div");
+  todo_item_left_div.append(todo_item_left_div_trash);
+  todo_item_left_div_trash.onclick = () => removeTask(todoId);
+
+  todo_item_left_div_trash.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                                         class="trash" viewBox="0 0 16 16">
                                         <path
                                             d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z" />
-                                    </svg>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
-                                    class="edit" viewBox="0 0 16 16">
-                                    <path
-                                        d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
-                                    <path fill-rule="evenodd"
-                                        d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
-                                    </svg>`;
+                                        </svg>`;
+
+  todo_item_left_div_edit.innerHTML += `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                      class="edit" viewBox="0 0 16 16">
+                                      <path
+                                          d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
+                                      <path fill-rule="evenodd"
+                                          d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
+                                      </svg>`;
 }
 
 function addTask() {
@@ -141,6 +158,60 @@ function addTask() {
   storageTaskInfo[index].task.push(newTask);
   localStorage.setItem("taskArr", JSON.stringify(storageTaskInfo));
   showTask(storageTaskInfo[index].task);
+  taskTitle.value = "";
+  taskTime.value = "";
+  countTasksFunction();
 }
 
-function taskDone() {}
+function taskDone(id) {
+  const dayIndex = storageTaskInfo.findIndex(
+    (item) => item.day === weekDayName
+  );
+  const todoIndex = storageTaskInfo[dayIndex].task.findIndex(
+    (item) => item.id == id
+  );
+  storageTaskInfo[dayIndex].task[todoIndex].isDone = true;
+  showTask(storageTaskInfo[dayIndex].task);
+  localStorage.setItem("taskArr", JSON.stringify(storageTaskInfo));
+}
+
+function countTasksFunction() {
+  countTasks.forEach((item) => {
+    storageTaskInfo.forEach((day) => {
+      if (day.day == item.dataset.day) {
+        item.innerText = day.task.length;
+      }
+    });
+  });
+}
+countTasksFunction();
+
+function editTask(id, taskTitle, taskTime) {
+  dayIndex = storageTaskInfo.findIndex((item) => item.day === weekDayName);
+  todoIndex = storageTaskInfo[dayIndex].task.findIndex((item) => item.id == id);
+  editTaskModal.style.display = "flex";
+  editedTaskTitle.value = taskTitle;
+  editedTaskTime.value = taskTime;
+}
+
+editSubmitBtn.addEventListener("click", editSubmit);
+function editSubmit() {
+  storageTaskInfo[dayIndex].task[todoIndex].taskName = editedTaskTitle.value;
+  storageTaskInfo[dayIndex].task[todoIndex].taskTime = editedTaskTime.value;
+  showTask(storageTaskInfo[dayIndex].task);
+  editTaskModal.style.display = "none";
+  localStorage.setItem("taskArr", JSON.stringify(storageTaskInfo));
+}
+
+function removeTask(id) {
+  const dayIndex = storageTaskInfo.findIndex(
+    (item) => item.day === weekDayName
+  );
+
+  const storageTaskInfoEdit = storageTaskInfo[dayIndex].task.filter(
+    (item) => item.id != id
+  );
+  storageTaskInfo[dayIndex].task = storageTaskInfoEdit;
+  showTask(storageTaskInfo[dayIndex].task);
+  localStorage.setItem("taskArr", JSON.stringify(storageTaskInfo));
+}
